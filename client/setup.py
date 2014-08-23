8fc9f65dcbe07832a6af7494d3b6208a8cf1245b
alfateam123 <alfateam123@gmail.com>
2014-08-23 09:22:19 -0400

    i just ____ it all

---
+++ client/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='CardsAgainstWeb',
+    version='0.1.0',
+    author='alfateam123',
+    author_email='alfateam123@gmail.com',
+    packages=['cardsagainstweb'],
+    scripts=['bin/play_cah'],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    license='../LICENSE',
+    description='CAH',
+    long_description=open('../README.txt').read(),
+    #install_requires=[
+    #],
+)
